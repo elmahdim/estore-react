018d10415a8d45fc7c8f1a9087694b40db5751f4
elmahdim <elmahdi.mahmoud@live.com>
2016-09-21 10:54:55 +0300

    set router querykey to false. add fontawesome icons from cdn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Catalog from './components/catalog/Catalog';
 // import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory }>
+  <Router history={browserHistory } queryKey={false}>
     <Route path='/' component={Catalog}></Route>
   </Router>,
   document.getElementById('root')
